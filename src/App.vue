cb860f9c0f60ef3609e6f3a6912198ed8b6ef191
xduduaraujo <xduduaraujo@gmail.com>
2022-04-05 20:41:23 -0300

    Arrumando redirecionamento de rotas

--- src/App.vue
+++ src/App.vue
@@ -1,91 +1,26 @@
 <script setup lang="ts">
 import { RouterView } from "vue-router";
+import UBScheduleHeader from "@organisms/UBScheduleHeader.vue";
 </script>
 
 <template>
-  <header>
-    <img alt="Ubook logo" src="@/assets/ubook.svg" />
-  </header>
-
-  <RouterView />
+	<UBScheduleHeader />
+	<RouterView />
 </template>
 
 <style>
 @import "@/assets/base.css";
 
 #app {
-  max-width: 1280px;
-  margin: 0 auto;
-  padding: 2rem;
+	max-width: 1280px;
+	margin: 0 auto;
+	padding: 1rem;
 
-  font-weight: normal;
+	font-weight: normal;
 }
 
 header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  body {
-    display: flex;
-    place-items: center;
-  }
-
-  #app {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    padding: 0 2rem;
-  }
-
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+	line-height: 1.5;
+	max-height: 100vh;
 }
 </style>
