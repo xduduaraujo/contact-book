3d5156675eb139060e12abf4bd2f5fb454f80afb
xduduaraujo <xduduaraujo@gmail.com>
2022-04-12 12:21:32 -0300

    Corrigindo formulario

--- src/components/molecules/UBContact.vue
+++ src/components/molecules/UBContact.vue
@@ -1,22 +1,21 @@
 <template>
-  <div class="ub-contact">
-    <div class="ub-contact-avatar">
-      <UBAvatar :alt="`${contact.name} Avatar`" width="24" height="24" :image="square" v-if="contact.name" />
-      <UBText :text="contact.name.charAt(0).toUpperCase()" class="ub-first-letter-of-name" />
-    </div>
-    <UBText :text="contact.name" class="ub-contact-info" />
-    <UBText :text="contact.email" class="ub-contact-info" />
-    <UBText :text="contact.telephone" class="ub-contact-info" />
-    <UBIcon alt="Edit Icon" :image="edit" width="16" height="16" class="ub-edit-icon" :onClick="() => {}" />
-    <UBIcon
-      alt="Delete Icon"
-      :image="deleteIcon"
-      width="16"
-      height="16"
-      class="ub-delete-icon"
-      :onClick="deleteContact"
-    />
-  </div>
+	<div class="ub-contact">
+		<div class="ub-contact-avatar">
+			<UBAvatar :alt="`${contact.name} Avatar`" width="24" height="24" :image="square" v-if="contact.name" />
+			<UBText :text="contact.name?.charAt(0).toUpperCase()" class="ub-first-letter-of-name" />
+		</div>
+		<UBText :text="contact.name" class="ub-contact-info" />
+		<UBText :text="contact.email" class="ub-contact-info" />
+		<UBText :text="contact.telephone" class="ub-contact-info" />
+		<UBIcon alt="Edit Icon" :image="edit" width="16" height="16" class="ub-edit-icon" :onClick="() => { }" />
+		<UBIcon
+			alt="Delete Icon"
+			:image="deleteIcon"
+			width="16"
+			height="16"
+			class="ub-delete-icon"
+			:onClick="deleteContact" />
+	</div>
 </template>
 
 <script lang="ts">
@@ -29,89 +28,89 @@ import deleteIcon from '@/assets/delete.svg';
 import UBText from '@atoms/UBText.vue';
 
 export default defineComponent({
-  name: 'UBContact',
-  components: { UBAvatar, UBText, UBIcon },
-  props: {
-    contact: {
-      type: Object,
-      required: true
-    }
-  },
-  setup() {
-    const deleteContact = inject('deleteContact') as Function;
+	name: 'UBContact',
+	components: { UBAvatar, UBText, UBIcon },
+	props: {
+		contact: {
+			type: Object,
+			required: true
+		}
+	},
+	setup() {
+		const deleteContact = inject('deleteContact') as Function;
 
-    return { square, deleteIcon, edit, deleteContact };
-  }
+		return { square, deleteIcon, edit, deleteContact };
+	}
 });
 </script>
 
 <style lang="scss" scoped>
 .ub-contact {
-  display: flex;
-  height: 40px;
-  border-radius: 4px;
-  border: solid 1px var(--ub-white);
+	display: flex;
+	height: 40px;
+	border-radius: 4px;
+	border: solid 1px var(--ub-white);
 }
 
 .ub-contact-info {
-  flex: 4;
-  margin: 12px 0 0 0;
-  text-align: left;
-  text-overflow: ellipsis;
-  white-space: nowrap;
-  overflow: hidden;
-  width: 100px;
-  font-size: 14px;
+	flex: 4;
+	margin: 12px 0 0 0;
+	text-align: left;
+	text-overflow: ellipsis;
+	white-space: nowrap;
+	overflow: hidden;
+	width: 100px;
+	font-size: 14px;
 }
 
 .ub-contact-avatar {
-  max-width: 48px;
-  flex: 1.5;
-  position: relative;
-  text-align: center;
-  color: white;
+	max-width: 48px;
+	flex: 1.5;
+	position: relative;
+	text-align: center;
+	color: white;
 }
 
 .ub-edit-icon {
-  max-width: 16px;
-  flex: 0.5;
-  position: relative;
-  text-align: center;
-  margin: 12px 12px 12px 12px;
+	max-width: 16px;
+	flex: 0.5;
+	position: relative;
+	text-align: center;
+	margin: 12px 12px 12px 12px;
 }
 
 .ub-delete-icon {
-  max-width: 16px;
-  flex: 0.5;
-  position: relative;
-  text-align: center;
-  margin: 12px 12px 12px 12px;
+	max-width: 16px;
+	flex: 0.5;
+	position: relative;
+	text-align: center;
+	margin: 12px 12px 12px 12px;
 }
 
 @media (max-width: 280px) {
-  .ub-delete-icon {
-    display: none;
-  }
+	.ub-delete-icon {
+		display: none;
+	}
 
-  .ub-edit-icon {
-    display: none;
-  }
+	.ub-edit-icon {
+		display: none;
+	}
 }
 
 .ub-first-letter-of-name {
-  color: white;
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  width: 13px;
-  font-family: Roboto;
-  font-size: 16px;
-  font-weight: normal;
-  font-stretch: normal;
-  font-style: normal;
-  line-height: normal;
-  letter-spacing: normal;
-  text-align: center;
+	color: white;
+	position: absolute;
+	top: 50%;
+	left: 50%;
+	transform: translate(-50%, -50%);
+	width: 13px;
+	font-family: Roboto;
+	font-size: 16px;
+	font-weight: normal;
+	font-stretch: normal;
+	font-style: normal;
+	line-height: normal;
+	letter-spacing: normal;
+	text-align: center;
 }
 </style>
