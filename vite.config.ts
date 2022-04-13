79957217eaa80033c2f672f11265874bf96bf3bd
xduduaraujo <xduduaraujo@gmail.com>
2022-04-12 21:41:15 -0300

    Adicionando base para gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,15 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-      '@atoms': fileURLToPath(new URL('./src/components/atoms', import.meta.url)),
-      '@molecules': fileURLToPath(new URL('./src/components/molecules', import.meta.url)),
-      '@organisms': fileURLToPath(new URL('./src/components/organisms', import.meta.url)),
-      '@templates': fileURLToPath(new URL('./src/components/templates', import.meta.url))
-    }
-  }
+	base: '/schedule-front/',
+	plugins: [vue()],
+	resolve: {
+		alias: {
+			'@': fileURLToPath(new URL('./src', import.meta.url)),
+			'@atoms': fileURLToPath(new URL('./src/components/atoms', import.meta.url)),
+			'@molecules': fileURLToPath(new URL('./src/components/molecules', import.meta.url)),
+			'@organisms': fileURLToPath(new URL('./src/components/organisms', import.meta.url)),
+			'@templates': fileURLToPath(new URL('./src/components/templates', import.meta.url))
+		}
+	}
 });
